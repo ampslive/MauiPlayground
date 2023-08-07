197f79e1eb1465d918e6033676aa010d94c92838
Amit Philips <amitphilips@live.com>
2023-08-07 17:56:02 +0530

    CollectionView ItemSource fix

--- MauiPlayground/MainPage.xaml.cs
+++ MauiPlayground/MainPage.xaml.cs
@@ -1,44 +1,27 @@
-﻿using System.Collections.ObjectModel;
+﻿using MauiPlayground.Models;
+using System.Collections.ObjectModel;
 
 namespace MauiPlayground;
 
 public partial class MainPage : ContentPage
 {
-	public ObservableCollection<Experiment> Experiments;
+    public ObservableCollection<Experiment> Experiments = new ObservableCollection<Experiment>();
 
-	public MainPage()
-	{
-		InitializeComponent();
-
-		this.Experiments = new ObservableCollection<Experiment>(); 
-
-		Experiments.Add(new Experiment("UI Switcher", "Display and Hide features based on a particular value. This is using Bit Masking techniques.", "thumbnail", "type", "page"));
-        BindingContext = new Experiment();
-    }
-}
-
-public class Experiment
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-	public string Description { get; set; }
-	public string Thumbnail { get; set; }
-	public string Type { get; set; }
-	public string PageUri { get; set; }
-
-    public Experiment()
+    public MainPage()
     {
-        Id = Guid.NewGuid();
-        Name = "Hi";
+        InitializeComponent();
+
+        //Experiments.Add(new Experiment("UI Switcher", "Display and Hide features based on a particular value. This is using Bit Masking techniques.", "thumbnail", "type", "page"));
+        BindingContext = Experiments;
     }
 
-    public Experiment(string name, string description, string thumbnail, string type, string pageUri)
+    protected override void OnAppearing()
     {
-		Id = Guid.NewGuid();
-		Name = name;
-		Description = description;
-		Thumbnail = thumbnail;
-		Type = type;
-		PageUri = pageUri;
+        base.OnAppearing();
+
+        Experiments.Add(new Experiment("UI Switcher", "Display and Hide features based on a particular value. This is using Bit Masking techniques.", "thumbnail", "type", "page"));
     }
+
+
 }
+    
---
+++ MauiPlayground/Models/Experiment.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiPlayground.Models;
+
+public class Experiment
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Thumbnail { get; set; }
+    public string Type { get; set; }
+    public string PageUri { get; set; }
+
+    public Experiment()
+    {
+        Id = Guid.NewGuid();
+        Name = "Hi";
+    }
+
+    public Experiment(string name, string description, string thumbnail, string type, string pageUri)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        Thumbnail = thumbnail;
+        Type = type;
+        PageUri = pageUri;
+    }
+}

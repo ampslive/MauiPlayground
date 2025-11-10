55d52e36b1db3ad195c81145dd7f75dc9676a305
Amit Philips <amit.philips@kantar.com>
2025-11-10 18:12:25 +0530

    Set referrer for Youtube video

--- src/MAUI9/MAUIPlayground/Pages/YouTube.xaml.cs
+++ src/MAUI9/MAUIPlayground/Pages/YouTube.xaml.cs
@@ -2,8 +2,46 @@ namespace MAUIPlayground.Pages;
 
 public partial class YouTube : ContentPage
 {
-	public YouTube()
-	{
-		InitializeComponent();
-	}
+    public YouTube()
+    {
+        InitializeComponent();
+
+        string htmlContent = @"
+<html>
+  <head>
+    <style>
+      * {
+      margin: 0;
+        padding: 0;
+    }
+      html, body {
+width: 100%;
+    height: 100%;
+        overflow: hidden;
+      }
+  iframe {
+    width: 100%;
+        height: 100%;
+        border: none;
+     display: block;
+      }
+    </style>
+  </head>
+  <body>
+    <iframe
+      src='https://www.youtube.com/embed/QbRBqTPz_g4?rel=0&fs=0&modestbranding=1'
+    frameborder='0'
+      allowfullscreen
+  ></iframe>
+  </body>
+</html>";
+
+        var htmlSource = new HtmlWebViewSource
+        {
+            Html = htmlContent,
+            BaseUrl = "https://amitphilips.com" // This sets the Referer
+        };
+
+        YoutubeWebView.Source = htmlSource;
+    }
 }
